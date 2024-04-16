f76b840b593203940eb95b1376603dad2b71ba23
Jana-Burri <jana.burri@hotmail.com>
2024-04-16 20:55:18 +0200

    Adjusted database tables and routes accordingly

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,15 +1,16 @@
 DROP TABLE if exists user;
-DROP TABLE if exists user_info;
-DROP TABLE if exists quotes; 
+DROP TABLE if exists questions; 
 DROP TABLE if exists games;
+DROP TABLE if exists user_info;
 
 CREATE TABLE `user_info`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `quotes`(
+CREATE TABLE `questions`(
     `question_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `result_points` INT NOT NULL,
+    `quote_id` INT NOT NULL,
+    `points` INT NOT NULL,
     `quote_text` VARCHAR(255) NOT NULL,
     `solution_text` VARCHAR(255) NOT NULL,
     `player_answer` VARCHAR(255) NOT NULL,
@@ -21,6 +22,6 @@ CREATE TABLE `games`(
     `game_total` INT NOT NULL
 );
 ALTER TABLE
-    `quotes` ADD CONSTRAINT `quotes_game_id_foreign` FOREIGN KEY(`game_id`) REFERENCES `games`(`game_id`) ON DELETE cascade;
+    `questions` ADD CONSTRAINT `questions_game_id_foreign` FOREIGN KEY(`game_id`) REFERENCES `games`(`game_id`) ON DELETE cascade;
 ALTER TABLE
     `games` ADD CONSTRAINT `games_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user_info`(`id`) ON DELETE cascade;
--- routes/games.js
+++ routes/games.js
@@ -19,6 +19,7 @@ router.get("/", async function (req, res, next) {
   //   .catch((err) => res.status(500).send(err));
 });
 
+// Get all the info on a specific game
 router.get("/:id", async function (req, res, next) {
   try {
     const id = req.params.id;
@@ -29,11 +30,12 @@ router.get("/:id", async function (req, res, next) {
   }
 });
 
-router.get("/:id/sum", async function (req, res) {
+// Get the game total for a specific game
+router.get("/:id/total", async function (req, res) {
   try {
     const id = req.params.id;
     const response = await db(
-      `select sum(q1 + q2 + q3 + q4 + q5) total from games where game_id = ${id};`
+      `select game_total from games where game_id = ${id};`
     );
     res.send(response.data);
   } catch (err) {
@@ -41,6 +43,86 @@ router.get("/:id/sum", async function (req, res) {
   }
 });
 
+// Inserting the amount of points I made for a question after having chosen an answer
+router.post("/", async function (req, res, next) {
+  try {
+    const {
+      quote_id,
+      points,
+      quote_text,
+      solution_text,
+      player_answer,
+      game_id,
+    } = req.body;
+
+    await db(
+      `insert into questions (quote_id, points, quote_text, solution_text, player_answer, game_id) values (${quote_id}, ${points}, "${quote_text}", "${solution_text}", "${player_answer}", ${game_id});`
+    );
+    const results = await db("SELECT * FROM questions;");
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Once the game is over: Update the game_total in my users_info with the sum of the points I made during the game
+router.put("/:id/game_total", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await db(
+      `UPDATE games SET game_total = (select sum(points) FROM questions WHERE game_id = ${id}) WHERE game_id = ${id};`
+    );
+    const results = await db("SELECT * FROM games ORDER BY game_id ASC;");
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Get specific answer that a certain player has given to a certain question
+router.get("/:game_id/:quote_id/player_answer", async (req, res) => {
+  try {
+    const { game_id, quote_id } = req.params;
+    const response = await db(
+      `SELECT player_answer FROM questions WHERE game_id = ${game_id} and quote_id = ${quote_id};`
+    );
+
+    res.send(response.data);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// Get solution to a certain question of a certain game
+router.get("/:game_id/:quote_id/solution_text", async (req, res) => {
+  try {
+    const { game_id, quote_id } = req.params;
+    const response = await db(
+      `SELECT solution_text FROM questions WHERE game_id = ${game_id} and quote_id = ${quote_id};`
+    );
+
+    res.send(response.data);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Get the amount of points for a specific question
+// q stands for the number of the question
+// Careful: Doesn't work anymore using the adjusted database!!!!
 router.get("/:id/:q", async function (req, res) {
   try {
     const { id, q } = req.params;
@@ -52,6 +134,7 @@ router.get("/:id/:q", async function (req, res) {
 });
 
 // the "/" below means, that I access my back-end at "/" (which btw stand for "/api/games" as defined in app.js in line 17),
+// Careful: Doesn't work anymore using the adjusted database!!!!
 router.post("/", async function (req, res, next) {
   try {
     const { q1, q2, q3, q4, q5 } = req.body; // VERY IMPORTANT: req.body is what gets sent from my front-end via (method: "x" ....) to my backend!
@@ -66,21 +149,6 @@ router.post("/", async function (req, res, next) {
     res.send(results.data);
   } catch (err) {
     res.status(500).send(err.message);
-  }
-});
-
-// ???????????
-router.put("/:id/:game_total", async (req, res) => {
-  try {
-    const { id, game_total } = req.params;
-
-    await db(`UPDATE games SET game_total = ${total} WHERE id = ${id};`);
-
-    const results = await db("SELECT * FROM games ORDER BY id ASC;");
-
-    res.send(results.data);
-  } catch (err) {
-    res.status(500).send(err);
   }
 });
 
